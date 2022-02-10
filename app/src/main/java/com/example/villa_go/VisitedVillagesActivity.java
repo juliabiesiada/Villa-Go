55024ad74fc2f8007180d29c621302e9c9fe8f2b
FlorianaPieprzyk <floriana.pieprzyk@u-psud.fr>
2022-02-11 00:39:41 +0100

    escargot game + previous work

--- app/src/main/java/com/example/villa_go/VisitedVillagesActivity.java
+++ app/src/main/java/com/example/villa_go/VisitedVillagesActivity.java
@@ -6,11 +6,15 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class VisitedVillagesActivity extends AppCompatActivity implements View.OnClickListener{
 
+    LinearLayout parisLinearLayout;
     ImageView parisImageView;
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +24,30 @@ public class VisitedVillagesActivity extends AppCompatActivity implements View.O
     }
 
     private void setupUI() {
-        parisImageView = findViewById(R.id.parisImageView);
-        parisImageView.setOnClickListener(this);
+        //parisImageView = findViewById(R.id.parisImageView);
+        //parisImageView.setOnClickListener(this);
+        parisLinearLayout = findViewById(R.id.visitedCityParis);
+        parisLinearLayout.setOnClickListener(this);
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()) {
-            case R.id.parisImageView:
-                Intent intent = new Intent(this, VillageActivity.class);
+            case R.id.visitedCityParis:
+                intent = new Intent(this, VillageActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.backButton:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            /*case R.id.parisImageView:
+                Intent intent = new Intent(this, VillageActivity.class);
+                startActivity(intent);*/
         }
     }
 }
