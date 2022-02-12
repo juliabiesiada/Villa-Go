34666f4d63bbd60326a5e2538e275e93e843078f
iTaghoury <idriss.taghoury@gmail.com>
2022-02-12 20:00:59 +0100

    Added layoutand java files for Match pictures and words game also added png images

---
+++ app/src/main/java/com/example/villa_go/MatchImagesAndWordsGame.java
@@ -0,0 +1,23 @@
+package com.example.villa_go;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MatchImagesAndWordsGame extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.match_images_and_words_activity);
+        setupUI();
+    }
+    private void setupUI() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
